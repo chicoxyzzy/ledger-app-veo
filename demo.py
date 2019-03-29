3bab9cba7d053889ca11e81609ef4d30fef4757d
Denis Voskvitsov <denis.voskvitsov@gmail.com>
2019-03-29 19:29:19 +0300

    Rewrite demo.py to python3 with ability to serialize and properly verify received signature

--- demo.py
+++ demo.py
@@ -20,13 +20,17 @@ import base64
 from ledgerblue.commException import CommException
 
 from secp256k1 import PublicKey
+from utils import serialize
+import json
+import codecs
+import binascii
 
 bipp44_path = ("80000000"
               +"00000000"
               +"00000000")
 
 dongle = getDongle(True)
-keys = dongle.exchange(bytes(("80010000FF"+ bipp44_path).decode('hex')))
+keys = dongle.exchange(codecs.decode("80010000FF"+ bipp44_path, 'hex'))
 publicKey = keys[:65]
 privateKey = keys[65:]
 
@@ -34,44 +38,56 @@ _textToSign = '["create_acc_tx","BNKQhfy4bRm3We9E+DXS7dhWp2avHSN2sIDuOlpJjWwl8Lo
 
 _textToSign = '["spend","BB3G8DfYtkZurxIxAGw2Y8ELhVx8WyjjqAdoVptOB+tW5cQsj93XGflHJBrFKpgEFWTUKKYKDM8GYPu4eUM+DCg=",4,60707,"BNKQhfy4bRm3We9E+DXS7dhWp2avHSN2sIDuOlpJjWwl8Lo1FdKfNhk63yTFlXe685URVRDfbXPY16HlXDqYagg=",900000,0]'
 
-
+_textToSign = '["spend","BB3G8DfYtkZurxIxAGw2Y8ELhVx8WyjjqAdoVptOB+tW5cQsj93XGflHJBrFKpgEFWTUKKYKDM8GYPu4eUM+DCg=",36,61657,"BHVfQWRx7I1xmlWw5KJ7l9ijq4tcobFCub68jsmCGaEQXSr/hi0Vpz8+wa0v8ytcTEUu4wJwTfC+7q6r+CPYS20=",10000000,0]'
 
 
 
 textToSign = _textToSign
 
-print ("%s, %d, %s" % (base64.b64encode(publicKey), len(publicKey), str(privateKey).encode('hex')))
+print ("\nPublic key: %s, length: %d\n" % (base64.b64encode(publicKey), len(publicKey)))
 
 try:
     offset = 0
-    while offset <> len(textToSign):
+    while offset != len(textToSign):
         if (len(textToSign) - offset) > 255:
             chunk = textToSign[offset : offset + 255]
         else:
             chunk = textToSign[offset:]
         if (offset + len(chunk)) == len(textToSign):
             p1 = 0x80
-            bip = bytes(bipp44_path).decode('hex')
+            bip = codecs.decode(bipp44_path, 'hex')
         else:
             p1 = 0x00
             bip = bytes('')
-        apdu = bytes("8002".decode('hex')) + chr(p1) + chr(0x00) + chr(len(chunk + bip)) + bytes(chunk) + bip
+
+        apdu = codecs.decode("8002", "hex")
+        apdu += bytes((p1, ))
+        apdu += bytes((0x00, ))
+        l = len(chunk) + len(bip)
+        apdu += bytes((l, ))
+        apdu += bytes(chunk, 'ascii')
+        apdu += bip
+
         signature = dongle.exchange(apdu)
         offset += len(chunk)
 
-    print "signature " + str(signature).encode('hex')
+        print ("\nSent chunk: " + binascii.hexlify(bytes(chunk, 'ascii')).decode())
+
+    print ("\nGot signature: " + binascii.hexlify(signature).decode())
     publicKey = PublicKey(bytes(publicKey), raw=True)
 
-    print "signature b64 " + base64.b64encode(signature)
+    print ("base64-encoded: " + base64.b64encode(signature).decode())
     signature = publicKey.ecdsa_deserialize(bytes(signature))
 
-    print "verified " + str(publicKey.ecdsa_verify(bytes(_textToSign), signature))
+    dataToSign = bytes(serialize(json.loads(textToSign)))
+    print ("\nSource tx: " + textToSign)
+    print ("\nIs signature verified? " + str(publicKey.ecdsa_verify(dataToSign, signature)))
     # fff = base64.b64decode("MEUCID3p7O1FIIWlN1kHAB3rSTK43HB8E9ZIN9h+sce1pDmcAiEA0P0DKOCEssJNqKsAEI+OuwBsU2AbYiCQfSxaveuxpWc=")
     # signature2 = publicKey.ecdsa_deserialize(bytes(fff))
     # print "verified " + str(publicKey.ecdsa_verify(bytes(_textToSign), signature2))
 
 except CommException as comm:
     if comm.sw == 0x6985:
-        print "Aborted by user"
+        print ("Aborted by user")
     else:
-        print "Invalid status " + comm.sw
+        print ("Invalid status " + comm.sw)
